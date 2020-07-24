81068d27aefabcc2f74a624bc0232ca5596ec387
porushmarwaha <porushmarwaha@gmail.com>
2020-07-25 00:50:53 +0530

    Refactoring of ShapeShifter.js

--- JS - Methods/Shapeshifter.js
+++ JS - Methods/Shapeshifter.js
@@ -1,7 +1,7 @@
-function shapeshiffter(text, left ,right){
+shapeShifter = (text, left ,right) =>{
     text = text.split('');
     for(let i = 0; i < left; i++)    text.push(text.shift());      
     for(let i = 0; i < right; i++)   text.unshift(text.pop());
     return text.join('');
 }
-console.log(shapeshiffter('Rahul' , 4 , 1));
+console.log(shapeShifter('Rahul' , 4 , 1));
